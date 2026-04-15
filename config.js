d2eff7b7eaa615620fabf73dbc4b9eb7ff619e3d
joshuazwoody-glitch <joshuazwoody@gmail.com>
2026-04-15 12:47:48 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  host: 'play.gemmc.net',           // A public server that allows offline mode
+  host: 'play.aternos.me',           // Reliable public server for offline testing
   port: 25565,
-  username: 'GrokTestBot',          // Fake name
+  username: 'GrokTestBot',           // You can change this name
   version: '1.21',
   auth: 'offline',
 
